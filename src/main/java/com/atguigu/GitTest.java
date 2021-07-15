71830205de270e6d025ce139185fb56baa34d3ce
Outro <Outro@163.com>
2021-07-15 20:02:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("pull test!!!!!!!!!");
+        System.out.println("pull test!!!!!!!!!");
+        System.out.println("pull test!!!!!!!!!");
+        System.out.println("pull test!!!!!!!!!");
+        System.out.println("pull test!!!!!!!!!");
+        System.out.println("pull test!!!!!!!!!");
+    }
+}
